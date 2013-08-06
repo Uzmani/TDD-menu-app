e1538769c443ee58ce8b1562aca12c56542efa5f
Abi Noda <a@abinoda.com>
2013-08-06 14:45:27 -0500

    removing superfluous examples

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -8,29 +8,11 @@ describe 'User' do
       # user visits the homepage
       # user can see the posts titles
     end
-    it "can not see bodies of the recent posts" do
-      pending
-      # given a user and a list of posts
-      # user visits the homepage
-      # user should not see the posts bodies
-    end
     it "can click on titles of recent posts and should be on the post show page" do
       pending
       # given a user and a list of posts
       # user visits the homepage
       # when a user can clicks on a post title they should be on the post show page
-    end
-    it "can not see the edit link" do
-      pending
-      # given a user and a list of posts
-      # user visits the homepage
-      # user should not see any edit links
-    end
-    it "can not see the delete link" do
-      pending
-      # given a user and a list of posts
-      # user visits the homepage
-      # user should not see any delete links
     end
   end
 
@@ -41,24 +23,6 @@ describe 'User' do
       # user visits the post show page
       # user should see the post title
       # user should see the post body
-    end
-    it "can not see the edit link" do
-      pending
-      # given a user and post(s)
-      # user visits the post show page
-      # user should not see the post edit link
-    end
-    it "can not see the published flag" do
-      pending
-      # given a user and post(s)
-      # user visits the post show page
-      # user should not see the published flag content
-    end
-    it "can not see the Admin homepage link" do
-      pending
-      # given a user and post(s)
-      # user visits the post show page
-      # user should not see the the admin homepage link
     end
   end
 end
